cd9178e4b83c9fbba51686539c7c3c18f4f22f4d
Spencer Curtis <spencercolecurtis@gmail.com>
2018-12-04 12:25:13 -0700

    Update EntryController with new (working) baseURL

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-let baseURL = URL(string: "https://journal-performance.firebaseio.com/")!
+let baseURL = URL(string: "https://journal-performance2.firebaseio.com/")!
 
 class EntryController {
         
